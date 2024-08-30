eea5229f84af622c76eb8e186878976eb911eb35
robb-meira <131679911+robb-meira@users.noreply.github.com>
2024-08-30 10:39:02 -0300

    Update aleatorio.js

--- part 2 do trimestre/js/aleatorio.js
+++ part 2 do trimestre/js/aleatorio.js
@@ -1,4 +1,4 @@
-const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+const nomes = ["Felipe", "Israel", "Roberta"];
 
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
